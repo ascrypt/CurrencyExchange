d16c93dc8d680cb2d94aba9f6e984806623947be
Afshin <Afshin@jibble.io>
2022-11-07 03:19:17 +0330

    Added polly for resiliency

--- CurrencyExchange/Program.cs
+++ CurrencyExchange/Program.cs
@@ -1,8 +1,10 @@
 using CurrencyExchange.Services.CurrencyExchangeService;
 using CurrencyExchange.Services.ExternalServices.ExchangeRatesApiIo;
 using CurrencyExchange.Services.ExternalServices.ExchangeRatesApiIO;
+using CurrencyExchange.Utils.Resiliency;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -21,8 +23,35 @@ builder.Services.Configure<ExchangeRatesApiIoOptions>(options =>
     configuration.GetSection("Services:ExchangeRatesApiIOSettings").Bind(options));
 builder.Services.AddTransient<ICurrencyExchange, CurrencyExchange.Services.CurrencyExchangeService.CurrencyExchange>();
 builder.Services.AddTransient<IExchangeRatesApiIoService, ExchangeRatesApiIoService>();
-builder.Services.AddHttpClient<ExchangeRatesApiIoService>();
+builder.Services.AddHttpClient<ExchangeRatesApiIoService>("ExchangeRatesApiIoService", hc =>
+    {
+        hc.BaseAddress = new Uri(configuration.GetSection("Services").GetValue<string>("ExchangeRatesApiIOSettings:BaseUrl"));
+        hc.DefaultRequestHeaders.Add(
+            "apikey", configuration.GetSection("Services")["ExchangeRatesApiIOSettings:ApiKey"]);
+        hc.Timeout =
+            TimeSpan.FromSeconds(
+                Convert.ToInt32(
+                    configuration.GetSection("ResiliencyPolicies")["Default:OptimisticTimeout"]));
+    })
+    .SetHandlerLifetime(TimeSpan.FromMinutes(
+        Convert.ToDouble(configuration.GetSection("ResiliencyPolicies")["Default:LifeTime"])))
+    .AddPolicyHandler(ResiliencyPolicies.GetDefaultRetryPolicy(configuration))
+    .AddPolicyHandler(ResiliencyPolicies.GetDefaultCircuitBreakerPolicy(configuration));
+var timeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(
+    TimeSpan.FromSeconds(10));
+var longTimeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(
+    TimeSpan.FromSeconds(30));
 
+var policyRegistry = builder.Services.AddPolicyRegistry();
+
+policyRegistry.Add("Regular", timeoutPolicy);
+policyRegistry.Add("Long", longTimeoutPolicy);
+
+builder.Services.AddHttpClient("PollyRegistryRegular")
+    .AddPolicyHandlerFromRegistry("Regular");
+
+builder.Services.AddHttpClient("PollyRegistryLong")
+    .AddPolicyHandlerFromRegistry("Long");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- CurrencyExchange/Services/ExternalServices/ExchangeRatesApiIo/ExchangeRatesApiIoService.cs
+++ CurrencyExchange/Services/ExternalServices/ExchangeRatesApiIo/ExchangeRatesApiIoService.cs
@@ -1,26 +1,31 @@
 ﻿using CurrencyExchange.DTOs;
 using CurrencyExchange.Services.ExternalServices.ExchangeRatesApiIo;
+using CurrencyExchange.Utils;
 using Microsoft.Extensions.Options;
 
 namespace CurrencyExchange.Services.ExternalServices.ExchangeRatesApiIO;
 
 public class ExchangeRatesApiIoService : IExchangeRatesApiIoService
 {
-    private readonly HttpClient _httpClient;
+    private readonly IHttpClientFactory _httpClientFactory;
     private readonly IOptionsMonitor<ExchangeRatesApiIoOptions> _exchangeRatesApiIoOptions;
 
-    public ExchangeRatesApiIoService(HttpClient httpClient,
+    public ExchangeRatesApiIoService(IHttpClientFactory httpClientFactory,
         IOptionsMonitor<ExchangeRatesApiIoOptions> exchangeRatesApiIoOptions)
     {
         _exchangeRatesApiIoOptions = exchangeRatesApiIoOptions;
-        _httpClient = httpClient;
-        _httpClient.BaseAddress = new Uri(_exchangeRatesApiIoOptions.CurrentValue.BaseUrl);
-        _httpClient.DefaultRequestHeaders.Add(
-            "apikey", _exchangeRatesApiIoOptions.CurrentValue.ApiKey);
+        _httpClientFactory = httpClientFactory;
     }
 
-    public async Task<GetExchangeRateResponseDto> GetExchangeRatesAsync(GetExchangeRateRequestDto request) =>
-        await _httpClient.GetFromJsonAsync<GetExchangeRateResponseDto>(
-            $"exchangerates_data/latest?symbols={request.Symbols}&base={request.Base}") ??
-        throw new InvalidOperationException();
+    public async Task<GetExchangeRateResponseDto> GetExchangeRatesAsync(GetExchangeRateRequestDto request)
+    {
+        var client = _httpClientFactory.CreateClient("ExchangeRatesApiIoService");
+        var generateExchangeRatesApiIoServiceUrl = client.BaseAddress?.AbsoluteUri
+            .CombineUrl($"exchangerates_data/latest?symbols={request.Symbols}&base={request.Base}");
+
+        var response = await _httpClientFactory.CreateClient("ExchangeRatesApiIoService")
+            .GetFromJsonAsync<GetExchangeRateResponseDto>(generateExchangeRatesApiIoServiceUrl);
+
+        return response ?? throw new InvalidOperationException();
+    }
 }

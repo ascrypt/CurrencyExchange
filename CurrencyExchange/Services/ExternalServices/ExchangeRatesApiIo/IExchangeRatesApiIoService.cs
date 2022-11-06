226a7a1f3e07678d03df224c6c8e07ec86ce632d
Afshin <Afshin@jibble.io>
2022-11-07 02:13:25 +0330

    Added CurrencyExchanger controller

---
+++ CurrencyExchange/Services/ExternalServices/ExchangeRatesApiIo/IExchangeRatesApiIoService.cs
@@ -0,0 +1,8 @@
+﻿using CurrencyExchange.DTOs;
+
+namespace CurrencyExchange.Services.ExternalServices.ExchangeRatesApiIO;
+
+public interface IExchangeRatesApiIoService
+{ 
+    Task<GetExchangeRateResponseDto> GetExchangeRatesAsync(GetExchangeRateRequestDto request);
+}
